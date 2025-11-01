9b6b61d597e56f7e84f6506cf77b0a7db67eb139
Daniel Waltz <danielbwaltz@gmail.com>
2025-11-01 18:22:47 -0400

    fix: update sxzz config and deps

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,15 @@
-import { composer, type ResolvableFlatConfig } from "eslint-flat-config-utils";
+import {
+  composer,
+  type FlatConfigComposer,
+  type ResolvableFlatConfig,
+} from "eslint-flat-config-utils";
 import type { BaseConfig, BaseConfigNames } from "./types.js";
 
 export function defineFlatConfigs<
   const TConfig extends BaseConfig = BaseConfig,
   const TConfigNames extends string = BaseConfigNames,
->(...configs: ResolvableFlatConfig<TConfig>[]) {
-  return composer<TConfig, TConfigNames>(...configs);
+>(
+  ...configs: ResolvableFlatConfig<TConfig>[]
+): FlatConfigComposer<TConfig, TConfigNames> {
+  return composer(...configs) as FlatConfigComposer<TConfig, TConfigNames>;
 }
